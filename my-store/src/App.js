5e24ac52dce212be15f737ef8c9fbd024d0d12fc
Broveku <gearhearjordan@gmail.com>
2021-10-05 18:04:52 -0700

    added route/switch

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import Header from './components/Header';
-import Signup from './components/Signup';
+import Header from './components/Header'
+import Signup from './components/Signup'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header />
-        
-        <Signup />
-       
+        <Header/>
+        <Switch>
+          <Route path='/signup'> 
+            <Signup/>
+          </Route>
+        </Switch>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
