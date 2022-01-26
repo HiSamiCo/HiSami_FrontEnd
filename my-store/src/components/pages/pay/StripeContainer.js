b15048b56de0eecca5a016e569514dc84fb5d376
JorshuaG <joshbgearheart@gmail.com>
2022-01-25 16:14:01 -1000

    added base functionality of stripe

--- my-store/src/components/pages/pay/StripeContainer.js
+++ my-store/src/components/pages/pay/StripeContainer.js
@@ -1,16 +1,17 @@
-import { Elements } from "@stripe/react-stripe-js"
-import { loadStripe } from "@stripe/stripe-js"
-import React from "react"
-import PaymentForm from "./PaymentForm"
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import PaymentForm from "./PaymentForm";
 
-const { REACT_APP_PUBLIC_KEY } = process.env
+const REACT_APP_PUBLIC_KEY =
+  "pk_test_51KGwawJRq6rRVvOveKLD6UYuJbt2ukhqFMYQDwplYv1h8vBtIkanKowA2gg2td0cSwI1X5CEI2PRdfoQ2J6lSyIp00by8eyzHf";
 
-const stripeTestPromise = loadStripe(REACT_APP_PUBLIC_KEY)
+const stripeTestPromise = loadStripe(REACT_APP_PUBLIC_KEY);
 
 export default function StripeContainer() {
-	return (
-		<Elements stripe={stripeTestPromise}>
-			<PaymentForm />
-		</Elements>
-	)
+  return (
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm />
+    </Elements>
+  );
 }
