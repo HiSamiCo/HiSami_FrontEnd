b8b0adf2dcf64c15c8a2a219e69f90676086b4f0
JorshuaG <joshbgearheart@gmail.com>
2022-02-08 14:09:27 -1000

    added file structure

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -1,31 +1,35 @@
-import Header from './components/common/Header'
-import Signup from './components/pages/signup/Signup'
-import Login from './components/pages/login/Login'
-import { Switch, Route } from 'react-router-dom'
-import Homepage from './components/pages/home/Homepage';
-import StripeContainer from './components/pages/pay/StripeContainer';
-import "./css/reset.css"
+import Header from "./components/common/Header";
+import Signup from "./components/pages/signup/Signup";
+import Login from "./components/pages/login/Login";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./components/pages/home/Homepage";
+import StripeContainer from "./components/pages/pay/StripeContainer";
+import "./css/reset.css";
+import AdminDashboard from "./components/admin/AdminDashboard";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Switch>
-        <Route path='/signup'> 
-          <Signup/>
+        <Route path="/signup">
+          <Signup />
         </Route>
-        <Route path='/login'> 
-          <Login/>
+        <Route path="/login">
+          <Login />
         </Route>
-        <Route path='/pay'>
-          <StripeContainer/>
+        <Route path="/pay">
+          <StripeContainer />
         </Route>
-        <Route path='/'>
-          <Homepage/>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/admindash">
+          <AdminDashboard />
         </Route>
       </Switch>
     </div>
   );
 }
 
-export default App
+export default App;
