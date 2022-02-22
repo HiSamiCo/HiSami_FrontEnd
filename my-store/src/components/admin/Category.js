545e2b4963e42fcd6f24d6e6efdf83b49f0fec74
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2022-02-21 19:02:22 -0600

    added toggle of category buttons

--- my-store/src/components/admin/Category.js
+++ my-store/src/components/admin/Category.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useMemo, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function Category(props) {
     const { category_name, category_id } = props.category
+    const [toggle, setToggle] = useState(false)
+    const { category_id: categoryId } = useParams()
+
+    const click = () => {
+        setToggle(!toggle)
+    }
+    const url = useMemo(() => {
+        return`/adminDash/${parseInt(categoryId) === category_id ? "" : category_id}`
+    }, [categoryId])
+
     // display a card for each cateogry
     // button for revealing products
     // focus effects greyscale background
     return (
-        <Link to={`/adminDash/${category_id}`}>
+        <NavLink to={url} onClick={click}>
             <div>
                 {category_name}
             </div>
-        </Link>
+        </NavLink>
     );
 }
